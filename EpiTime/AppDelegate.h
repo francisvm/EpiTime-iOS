c7eda1106813d7b25cefdc64da0b707d346445b0
Francis Visoiu Mistrih <thegameg1@yahoo.com>
2014-10-06 22:08:35 +0200

    Initial commit

---
+++ EpiTime/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EpiTime
+//
+//  Created by Francis Visoiu Mistrih on 06/10/2014.
+//  Copyright (c) 2014 EpiTime. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
