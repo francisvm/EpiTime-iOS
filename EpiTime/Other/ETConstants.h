a9fd15883bafb8f0b32fd5036b4a7c7e2af1b8f0
Francis Visoiu Mistrih <visoiu_f@epita.fr>
2014-12-19 23:26:06 +0100

    Add theme colors as constants

--- EpiTime/Other/ETConstants.h
+++ EpiTime/Other/ETConstants.h
@@ -8,6 +8,10 @@
 
 #import <Foundation/Foundation.h>
 
+#define RED [UIColor colorWithRed:0.753 green:0.224 blue:0.169 alpha:1.000]
+#define GREEN [UIColor colorWithRed:0.110 green:0.502 blue:0.275 alpha:1.000]
+#define BLUE [UIColor colorWithRed:0.192 green:0.227 blue:0.271 alpha:1.000]
+
 extern NSString *const APP_NAME;
 extern NSString *const DAY_TABLE_VIEW_CONTROLLER;
 extern NSString *const BASE_URL_WEEKS;
