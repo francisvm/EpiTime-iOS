1f23857c173b768f34583d0550108c5155c75594
Francis Visoiu Mistrih <visoiu_f@epita.fr>
2014-12-21 17:46:35 +0100

    Add "no classes today"

--- EpiTime/Day/ETDayTableViewController.m
+++ EpiTime/Day/ETDayTableViewController.m
@@ -11,6 +11,7 @@
 #import "ETCourseItem.h"
 #import "ETTools.h"
 #import "ETAPI.h"
+#import "ETConstants.h"
 
 #import "XMLDictionary.h"
 
@@ -42,7 +43,7 @@
     if (!self.dayLabel.text)
         self.dayLabel.text = @"...";
 
-    self.currentTask = [ETAPI fetchWeek:self.weekIndex ofGroup:[ETTools currentGroup] viewController:self completion:^(NSDictionary *recievedData, ETWeekItem *week) {
+    self.currentTask = [ETAPI fetchWeek:self.weekIndex ofGroup:[[ETTools currentGroup] stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding] viewController:self completion:^(NSDictionary *recievedData, ETWeekItem *week) {
         self.day = week.days[self.index];
         self.dateLabel.text = [ETTools humanDateFromDate:self.day.date];
         self.dayLabel.text = [ETTools weekDayFromDate:self.day.date];
@@ -70,7 +71,7 @@
         return;
     }
 
-    self.currentTask = [ETAPI fetchWeek:self.weekIndex ofGroup:[ETTools currentGroup] viewController:self completion:^(NSDictionary *recievedData, ETWeekItem *week) {
+    self.currentTask = [ETAPI fetchWeek:self.weekIndex ofGroup:[[ETTools currentGroup] stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding] viewController:self completion:^(NSDictionary *recievedData, ETWeekItem *week) {
         self.day = week.days[self.index];
         self.dateLabel.text = [ETTools humanDateFromDate:self.day.date];
         self.dayLabel.text = [ETTools weekDayFromDate:self.day.date];
@@ -103,6 +104,28 @@
     cell.endingLabel.text = [ETTools timeStringFromMinutes:(course.hour + course.duration) * 15];
 
     return cell;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
+    return self.day.courses.count ? 0 : 100;
+}
+
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
+    if (!self.day.courses.count) {
+        UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 60)];
+        label.textAlignment = NSTextAlignmentCenter;
+        label.textColor = [UIColor whiteColor];
+        label.alpha = 0.5;
+        label.text = @"No classes today";
+
+        UIImageView *logo = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"loading_1"]];
+        logo.frame = CGRectMake(self.view.frame.size.width / 2 - logo.frame.size.width / 2, label.frame.size.height, logo.frame.size.width, logo.frame.size.height);
+        UIView *wrapper = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, [self tableView:tableView heightForHeaderInSection:section])];
+        [wrapper addSubview:label];
+        [wrapper addSubview:logo];
+        return wrapper;
+    }
+    return nil;
 }
 
 @end
