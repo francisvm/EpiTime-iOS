cd18f2da4bef98dceca502df989bdf96f03e2401
Francis Visoiu Mistrih <visoiu_f@epita.fr>
2015-06-08 01:39:38 +0200

    Add ETDatePickerView, a date picker using FVCustomAlertView

---
+++ EpiTime/Day/ETDatePickerView.h
@@ -0,0 +1,13 @@
+//
+//  ETDatePickerView.h
+//  EpiTime
+//
+//  Created by Francis Visoiu Mistrih on 08/06/2015.
+//  Copyright (c) 2015 EpiTime. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ETDatePickerView : UIView
+
+@end
---
+++ EpiTime/Day/ETDatePickerView.m
@@ -0,0 +1,21 @@
+//
+//  ETDatePickerView.m
+//  EpiTime
+//
+//  Created by Francis Visoiu Mistrih on 08/06/2015.
+//  Copyright (c) 2015 EpiTime. All rights reserved.
+//
+
+#import "ETDatePickerView.h"
+
+@implementation ETDatePickerView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
